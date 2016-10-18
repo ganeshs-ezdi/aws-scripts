b224b4321bbba9b98d39eba9d420a4073801d836
neerajp-ezdi <neeraj.p@ezdi.us>
2016-10-18 13:55:39 +0530

    Added some stop script;

---
+++ start-lk.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sudo service logstash start
+sudo service elasticsearch start
+sudo service kibana start
+
---
+++ stop-only-logger.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+pgrep 'python.*random-logger.py'
+pkill 'python.*random-logger.py'
+
