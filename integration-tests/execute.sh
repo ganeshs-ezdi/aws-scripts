69b15924145fd233ee9358d5e485e33e1e65a62d
ganeshs-ezdi <ganesh.s@ezdi.us>
2016-10-25 13:11:44 +0530

    Added script to execute the command on a remote host

---
+++ integration-tests/execute.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+PROJECT_TO_RUN=$1
+HOST=$2
+USERTOLOG="ubuntu"
+PEMFILE=~/.keys/cac-int-ops.pem
+
+# remove script name from the arguments
+shift 1
+
+if [[ -z "$SCRIPT_NAME" ]]
+then
+	echo "Please give the script name and the parameters"
+	exit 0
+fi
+
+# Unable to find pem file
+if [[ ! -f "$PEMFILE" ]]
+then
+	echo "Unable to find pem file"
+	echo "PEMFILE=$PEMFILE"
+	exit 0
+fi
+
+ssh -i $PEMFILE $USERTOLOG@$HOST "
+	SCRIPT_URL=https://github.com/ganeshs-ezdi/aws-scripts.git
+	SCRIPT_DIR=$(basename $SCRIPT_URL .git)
+
+	if [[ ! -d "$SCRIPT_DIR" ]]
+	then
+		echo Git cloning the URL=$SCRIPT_URL
+		git clone $SCRIPT_URL
+	fi
+	git pull
+
+	cd $SCRIPT_DIR/integration-tests
+	echo Working Dir --$PWD--
+	PROJECT=$PROJECT_TO_RUN ./runproject.sh
+"
+
+echo 'Done'
